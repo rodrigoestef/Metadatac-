8d7e5a7a3c10f5c24ee3dac6a179772bba812f3d
rodrigoestef <rodrigoestef@gmail.com>
2021-04-03 17:03:41 -0300

    Adc teste para sobrescrever chave

--- test/teste.cpp
+++ test/teste.cpp
@@ -8,4 +8,7 @@ int main()
     meta->set((char *)"sobrenome", (void *)"estef");
     printf("%s\n", meta->get((char *)"nome"));
     printf("%s\n", meta->get((char *)"sobrenome"));
+    //overwrite test
+    meta->set((char *)"nome", (void *)"abcde");
+    printf("%s\n", meta->get((char *)"nome"));
 }
